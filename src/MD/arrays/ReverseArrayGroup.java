b20b25c8cfdfd3fba0724c13fcc55ad47386a303
vijayr <rsaivijaykrishna@gmail.com>
2020-11-27 22:38:08 +0530

    commit

--- src/MD/arrays/ReverseArrayGroup.java
+++ src/MD/arrays/ReverseArrayGroup.java
@@ -1,4 +1,18 @@
 package MD.arrays;
 
 public class ReverseArrayGroup {
+    public static void main(String[] args)
+    {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        int k = 3;
+        arr=reverse(arr, k);
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+    public static int[] revers(int[ ]arr,int k){
+        int n = arr.length;
+
+
+    }
+
 }
