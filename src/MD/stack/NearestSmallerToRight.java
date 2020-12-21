e333d2220fbe3c1e9e899612cd6b7a411eb36f46
vijayr <rsaivijaykrishna@gmail.com>
2020-12-22 01:18:43 +0530

    NearestSmallerToRight

---
+++ src/MD/stack/NearestSmallerToRight.java
@@ -0,0 +1,59 @@
+package MD.stack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class NearestSmallerToRight {
+    public static void main(String[] args) {
+        int arr[]={4,5,2,10,8}; //o/p -1 4 -1 2 2
+        int size=arr.length;
+        ArrayList<Integer> trr=findNearestSmallerToRight(arr,size);
+        Collections.reverse(trr);
+        System.out.println(trr);
+
+        ArrayList<Integer> crr=findNearestSmallerToRightTwo(arr,size);
+        Collections.reverse(crr);
+        System.out.println(crr);
+    }
+
+    private static ArrayList<Integer> findNearestSmallerToRightTwo(int[] arr, int size) {
+        ArrayList<Integer> v=new ArrayList<>();
+        Stack<Integer> s=new Stack<>();
+
+        for(int i=size-1;i>=0;i--){
+            while(s.size()>0 && s.peek()>=arr[i])
+                s.pop();
+            if(s.isEmpty())
+                v.add(-1);
+            else if(s.size()>0 && s.peek()<arr[i])
+                v.add(s.peek());
+
+            s.push(arr[i]);
+        }
+        return v;
+    }
+
+    private static ArrayList<Integer> findNearestSmallerToRight(int[] arr, int size) {
+        ArrayList<Integer> v=new ArrayList<>();
+        Stack<Integer> s=new Stack<>();
+
+        for(int i=size-1;i>=0;i--){
+            if(s.isEmpty())
+                v.add(-1);
+            else if(s.size()>0 && s.peek()<arr[i])
+                v.add(s.peek());
+            else if(s.size()>0 && s.peek()>=arr[i]){
+                while(s.size()>0 && s.peek()>=arr[i])
+                    s.pop();
+                if(s.isEmpty())
+                    v.add(-1);
+                else if(s.size()>0 && s.peek()<arr[i])
+                    v.add(s.peek());
+            }
+            s.push(arr[i]);
+        }
+        return v;
+    }
+
+}
