214e9d735d9f5962d33ae37c58c1557fdc4c2047
vijayr <rsaivijaykrishna@gmail.com>
2021-01-03 20:43:23 +0530

    NextAlphabet

--- src/MD/binarySearch/NextAlphabet.java
+++ src/MD/binarySearch/NextAlphabet.java
@@ -4,9 +4,26 @@ public class NextAlphabet{
     public static void main(String[] args) {
         char[] arr={'a','c','f','h'};
         System.out.println("The next alphabet after c is "+findNextAlphabet(arr,'c'));
+        System.out.println("The next alphabet after c is "+findNextAlphabet2(arr,'c'));
 
     }
     public static char findNextAlphabet(char[] arr,char key){
+        int start=0;int end=arr.length-1;int mid=0;char res='*';
+        while (start<=end){
+            mid=start+(end-start)/2;
+            if(arr[mid]==key)
+                start=mid+1;
+            else if(arr[mid]<key)
+                start=mid+1;
+            else if(arr[mid]>key){
+                res=arr[mid];
+                end=mid-1;
+            }
+        }
+        return res;
+    }
+
+    public static char findNextAlphabet2(char[] arr,char key){
         int start=0;int end=arr.length-1;int mid=0;char res='*';
         while (start<=end){
             mid=start+(end-start)/2;
@@ -18,5 +35,4 @@ public class NextAlphabet{
         }
         return res;
     }
-    
 }
