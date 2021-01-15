927885abbddfa04cb928a86f4b83bfe8c0ef95c8
vijayr <rsaivijaykrishna@gmail.com>
2021-01-16 00:29:04 +0530

    SubsetSum

--- src/MD/dynamicProgramming/knapsack/ItemValue01KnapsackTopDown.java
+++ src/MD/dynamicProgramming/knapsack/ItemValue01KnapsackTopDown.java
@@ -23,7 +23,7 @@ public class ItemValue01KnapsackTopDown {
         }
 
         for(int i=1;i<n+1;i++){
-            for(int j=0;j<W+1;j++){
+            for(int j=1;j<W+1;j++){
                 if(wt[i-1]<=j)
                     t[i][j]=Math.max((val[i-1]+t[i-1][j-wt[i-1]]),t[i-1][j]);
                  else
--- src/MD/dynamicProgramming/knapsack/SubsetSum.java
+++ src/MD/dynamicProgramming/knapsack/SubsetSum.java
@@ -1,7 +1,41 @@
 package MD.dynamicProgramming.knapsack;
 
+import java.util.Arrays;
+
 public class SubsetSum {
     public static void main(String[] args) {
+        int[] arr={2,3,7,8,10};
+        int sum=11;
+        System.out.println("can we form a subset? "+isThereASubset(arr,sum));
 
+    }
+    public static boolean isThereASubset(int[] arr,int sum){
+        int n=arr.length+1;
+        int m=sum+1;
+        boolean[][] t=new boolean[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0)
+                    t[i][j]=false;
+                if(j==0)
+                    t[i][j]=true;
+            }
+        }
+
+        /*for (boolean[] b:t) {
+            System.out.println(Arrays.toString(b));
+        }*/
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(arr[i-1]<=j)
+                    t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
+                else
+                    t[i][j]= t[i-1][j];
+            }
+        }
+
+        return t[n-1][sum];
     }
 }
