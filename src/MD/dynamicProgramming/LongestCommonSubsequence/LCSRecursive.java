a85df3599a8a91b2be9a64a7f62cab18b587ba53
vijayr <rsaivijaykrishna@gmail.com>
2021-01-29 21:41:47 +0530

    LCSRecursive

---
+++ src/MD/dynamicProgramming/LongestCommonSubsequence/LCSRecursive.java
@@ -0,0 +1,25 @@
+package MD.dynamicProgramming.LongestCommonSubsequence;
+
+/*Longest common subsequence*/
+public class LCSRecursive {
+    public static void main(String[] args) {
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+
+        char[] X=s1.toCharArray();
+        char[] Y=s2.toCharArray();
+        int m = X.length;
+        int n = Y.length;
+
+        System.out.println("Length of LCS is "+findLCS(X,Y,m,n));
+    }
+
+    private static int findLCS(char[] X,char[] Y,int m,int n){
+        if(m==0||n==0)
+            return 0;
+        if(X[m-1]==Y[n-1])
+            return 1+findLCS(X,Y,m-1,n-1);
+        else
+            return Math.max(findLCS(X,Y,m,n-1),findLCS(X,Y,m-1,n));
+    }
+}
