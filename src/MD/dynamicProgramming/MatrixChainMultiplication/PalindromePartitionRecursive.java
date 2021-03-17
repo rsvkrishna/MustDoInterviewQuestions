1bf1c04ae42d7219f143b89868332e8424b24659
vijayr <rsaivijaykrishna@gmail.com>
2021-03-17 13:12:13 +0530

    PalindromePartitionRecursive

--- src/MD/dynamicProgramming/MatrixChainMultiplication/PalindromePartitionRecursive.java
+++ src/MD/dynamicProgramming/MatrixChainMultiplication/PalindromePartitionRecursive.java
@@ -2,17 +2,17 @@ package MD.dynamicProgramming.MatrixChainMultiplication;
 
 public class PalindromePartitionRecursive {
     public static void main(String[] args) {
-        String str = "ababbbabbababa";
+        String str = "abbb";//"ababbbabbababa";
         System.out.println("Min cuts needed for "
-                + "Palindrome Partitioning is " + minPartition(str,0,str.length()-1));
+                + "Palindrome Partitioning is " + minPalPartition(str,0,str.length()-1));
     }
-    private static int minPartition(String s, int i, int j){
+    private static int minPalPartition(String s, int i, int j){
         if(i>=j || isPalindrome(s,i,j))
             return 0;
         int minValue=Integer.MAX_VALUE;
         for(int k=i;k<=j-1;k++){
-            int tempAns= minPartition(s,i,k)+1+
-                    minPartition(s,k+1,j);
+            int tempAns= minPalPartition(s,i,k)+1+
+                    minPalPartition(s,k+1,j);
 
             minValue=Math.min(minValue,tempAns);
         }
