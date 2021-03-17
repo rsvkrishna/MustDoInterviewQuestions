08e79d5f1d8e9dd0d68ae00870b1932902769079
vijayr <rsaivijaykrishna@gmail.com>
2021-03-17 13:12:23 +0530

    PalindromePartitionMemoize

---
+++ src/MD/dynamicProgramming/MatrixChainMultiplication/PalindromePartitionMemoize.java
@@ -0,0 +1,43 @@
+package MD.dynamicProgramming.MatrixChainMultiplication;
+
+import java.util.Arrays;
+
+public class PalindromePartitionMemoize {
+    private static int[][] t=new int[100][100];
+    public static void main(String[] args) {
+        String str = "abbb";//"ababbbabbababa";
+        for (int[] a:t) {
+            Arrays.fill(a,-1);
+        }
+        System.out.println("Min cuts needed for "
+                + "Palindrome Partitioning is " + minPalPartition(str,0,str.length()-1));
+
+    }
+    private static int minPalPartition(String s,int i,int j){
+        if(i>=j||isPalindrome(s,i,j))
+            return 0;
+        if(t[i][j]!=-1)
+            return t[i][j];
+
+        int minValue=Integer.MAX_VALUE;
+        int tempAns=0;
+        for(int k=i;k<=j-1;k++){
+            tempAns=minPalPartition(s,i,k)+1+
+                    minPalPartition(s,k+1,j);
+
+            minValue=Math.min(tempAns,minValue);
+        }
+
+        return t[i][j]=minValue;
+    }
+    private static boolean isPalindrome(String s,int i,int j){
+        while(i < j)
+        {
+            if(s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
