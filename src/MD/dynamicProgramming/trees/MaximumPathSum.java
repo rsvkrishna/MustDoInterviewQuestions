48e63f23a4102bf798eb539e1991edf5b1f70e69
vijayr <rsaivijaykrishna@gmail.com>
2021-03-29 00:05:06 +0530

    MaximumPathSum

--- src/MD/dynamicProgramming/trees/MaximumPathSum.java
+++ src/MD/dynamicProgramming/trees/MaximumPathSum.java
@@ -13,9 +13,20 @@ public class MaximumPathSum {
         root.right.right = new TreeNode(5);
         root.right.right.left = new TreeNode(25);
 
+        TreeNode root2 = new TreeNode(10);
+        root2.left = new TreeNode(10);
+        root2.left.left = new TreeNode(20);
+        root2.left.right = new TreeNode(-30);
+
+        root2.right = new TreeNode(20);
+        root2.right.left = new TreeNode(-10);
+        root2.right.right = new TreeNode(5);
+        root2.right.right.left = new TreeNode(25);
+
 
         maxSum=Integer.MIN_VALUE;
-        System.out.println("Maximum Path Sum between 2 leafs " + maxPathSum(root));
+        System.out.println("Maximum Path Sum " + maxPathSum(root));
+        System.out.println("Maximum Path Sum " + maxPathSum2(root2));
     }
     private static int maxPathSum(TreeNode root){
         if(root==null)
@@ -33,5 +44,24 @@ public class MaximumPathSum {
             maxSum=Math.max(maxSum,root.val+left+right);
 
         return maxSum;
+    }
+    private static int maxPathSum2(TreeNode root){
+        if(root==null)
+            return 0;
+
+        if(root.left==null && root.right==null)
+            return root.val;
+
+        int left=maxPathSum(root.left);
+        int right=maxPathSum(root.right);
+
+        int tempAns=Math.max(root.val,Math.max(left,right));
+        int ans=Math.max(tempAns,root.val+left+right);
+        int res=Math.max(tempAns,ans);
+
+       /* if(left!=0 && right!=0)
+            maxSum=Math.max(maxSum,root.val+left+right);*/
+
+        return tempAns;
     }
 }
